76292b38e5e60880f08ad59930794ebb031f823c
mrbunroeun <bunroeunhas@gmail.com>
2026-08-15 23:48:56 +0700

    added the favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="@yield('description', 'Laravel Application')">
 
     <title>@yield('title', config('app.name', 'Laravel'))</title>
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
